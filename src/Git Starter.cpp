abcf9345f4d45641831d9456f518e6b9fb7188c7
sidneyli <liteng0219@gmail.com>
2015-05-14 00:56:14 -0400

    hello world v1

---
+++ src/Git Starter.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Git.cpp
+// Author      : Sean
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << sin(60);
+	return 0;
+}
